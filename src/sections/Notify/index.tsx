d1724e7e48dfc4f78ce1e426e40f7a8a3966efdb
jessicamlee <jessicamlee25@gmail.com>
2023-05-12 17:55:26 -0700

    Update Notify/CTA section copy & styles
    - Maintained similar format to Notify section from landing-2022

--- src/sections/Notify/index.tsx
+++ src/sections/Notify/index.tsx
@@ -3,6 +3,8 @@ import { StaticImage } from 'gatsby-plugin-image';
 import { useState } from 'react';
 import Highlight from '../../components/Highlight';
 import PageSection from '../../components/PageSection';
+import cx from 'classnames';
+import InputButton from '../../components/InputButton';
 import {
   container,
   text,
@@ -10,38 +12,99 @@ import {
   headline,
   image,
   apply,
+  textHighlight,
+  ctaHeading,
+  faqCta,
 } from './Notify.module.scss';
 
 function Notify() {
-  const [email, setEmail] = useState('');
+  // const [emailInput, setEmailInput] = useState({email: ''});
+  // const [submitting, setSubmitting] = useState(false);
+
+  // Email Submission
+  // const onSubmit = async () => {
+  //   const id = toast.loading('Loading...');
+  //   try {
+  //     const { response } = ApiService.subscribe(emailInput, 'subscribe', 'reset');
+  //     toast.success(await response, {id});
+  //     setEmailInput({email: ''});
+  //   } catch (err) {
+  //     switch ((err as any).name) {
+  //       case 'AbbortError':
+  //         break;
+  //       case 'ApiServiceError':
+  //         toast.error((err as ApiServiceError).getHumanError(), { id });
+  //         console.error(err);
+  //         break;
+  //       default:
+  //         toast.error('Unexpected error. Please try again later', { id });
+  //         console.error(err);
+  //         break;
+  //     }
+  //   }
+    
+  //   // To validate provided email address
+  //   if (!/\S+@\S+\.\S+/.test(emailInput.email)) {
+  //     toast.error("Please enter a valid email address");
+  //     return;
+  //   };
+  // };
+
   return (
     <PageSection className={root} containerClassName={container} id='notify'>
       <StaticImage
-        src='../../images/laptop.png'
+        src='../../images/Rocket.svg'
         alt='laptop'
         quality={100}
         className={image}
       />
       <div className={headline}>
         <Typography
-          className={text}
-          textColor='primary-700'
+          className={cx(text, ctaHeading)}
+          textColor='neutral-50'
           textType='heading2'
           as='h2'
+          // TODO: Update copy with "Ready to be a hacker?" with textHighlight on "hacker?" when applications open
         >
-          Applications are&nbsp;
-          <Highlight highlightColor='warning-400'>now open!</Highlight>
+          Applications open&nbsp;
+          <span className={textHighlight}>soon</span>
         </Typography>
-        <Typography
+        {/* <Typography
           className={text}
-          textColor='primary-700'
+          textColor='neural-50'
           textType='heading4'
           as='p'
+
+          // TODO: Update when applications open
         >
-          Don't miss out. Apply below and we look forward to seeing you this
-          summer!
-        </Typography>
+          Let's hack, learn, collaborate, and meet new people. Applications close <sp an>[deadline here in format Month day]</span>
+        </Typography> */}
       </div>
+      <InputButton
+        label='Enter email'
+        name='Enter email'
+        buttonText='Notify me'
+        // onSubmit={(e) => {
+        //   e.preventDefault();
+        //   setSubmitting(true);
+        //   onSubmit();
+        //   return false;
+        // }}
+        // onChange={(e) => {
+        //   setEmailInput({
+        //     ...inputProps,
+        //     [e.currentTarget.name]: e.currentTarget.value.slice(0, 200),
+        //   });
+        // }}
+       >
+        <Button
+          type='submit'
+        >
+          Notify me
+        </Button>
+      </InputButton>
+      {/* 
+      TODO:  Update later when applications open:
       <Button
         href='https://dash.hackthe6ix.com'
         rel='noreferrer noopener'
@@ -50,18 +113,18 @@ function Notify() {
         as={BasicLink}
       >
         Apply Now
-      </Button>
+      </Button> */}
       <Typography
-        className={text}
-        textColor='primary-700'
+        className={cx(text, faqCta)}
+        textColor='neutral-50'
         textType='paragraph1'
         as='p'
       >
-        Got questions? Check out our{' '}
-        <BasicLink linkStyle='styled' href='#faq'>
+        Have questions? Check out our{' '}
+        <BasicLink linkStyle='styled' href='#faq' className={textHighlight}>
           FAQ
         </BasicLink>{' '}
-        section!
+        section.
       </Typography>
     </PageSection>
   );
