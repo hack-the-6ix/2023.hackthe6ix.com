ea311c3efb87fb6cf6a5cf610f4a5c84e10aaa7e
David Hui <opensource@davidhui.ca>
2023-08-07 17:49:18 -0400

    add current sponsors

--- src/sections/PastSponsors/PastSponsors.data.ts
+++ src/sections/PastSponsors/PastSponsors.data.ts
@@ -11,92 +11,94 @@ type PastSponsorGroup = {
 };
 const pastSponsors: PastSponsorGroup[] = [
   {
-    size: 25.75,
+    size: 30,
+    gap: 1,
+    tier: "platinum",
+    items: [
+      {
+        name: 'TMU',
+        fileName: 'tmu.webp',
+        url: 'https://www.torontomu.ca/cs/',
+      }
+    ],
+  },
+  {
+    size: 18,
     gap: 2,
     tier: "gold",
     items: [
       {
-        name: 'Buf',
-        fileName: 'buf.webp',
-        url: 'https://buf.build/',
-      },
-      {
-        name: 'Wealthsimple',
-        fileName: 'wealthsimple.webp',
-        url: 'https://wealthsimple.com',
-      },
-    ],
-  },
-  {
-    size: 13.5,
-    gap: 2,
-    tier: "silver",
-    items: [
-      {
-        name: 'Staples Studio',
-        fileName: 'staplesstudio.webp',
-        url: 'https://studio.staples.ca/studio/torontocorktown',
-        // offset: -25
-      },
-      {
-        name: 'Ontario Teachers Pension Plan',
-        fileName: 'otpp.webp',
-        url: 'https://www.otpp.com/en-ca/careers',
-      },
-    ],
-  },
-  {
-    size: 8.25,
-    gap: 2,
-    tier: "bronze",
-    items: [
-      {
-        name: 'PWC',
-        fileName: 'pwc.webp',
-        url: 'https://www.pwc.com/ca/en/careers/campus-recruiting/job-search.html',
-      },
-      {
         name: 'FDM',
         fileName: 'fdm.webp',
-        url: 'https://apply.fdmgroup.com/?chkCategory=1&chkCategory=3&lstRegion=10',
-      },
-      {
-        name: 'AMD',
-        fileName: 'amd.webp',
-        url: 'https://www.amd.com/en',
-      },
-      {
-        name: 'Balsamiq',
-        fileName: 'balsamiq.webp',
-        url: 'https://balsamiq.com/wireframes/',
-      },
+        url: 'https://www.fdmgroup.com/',
+      }
     ],
   },
+  // {
+  //   size: 13.5,
+  //   gap: 2,
+  //   tier: "silver",
+  //   items: [
+  //     {
+  //       name: 'Staples Studio',
+  //       fileName: 'staplesstudio.webp',
+  //       url: 'https://studio.staples.ca/studio/torontocorktown',
+  //       // offset: -25
+  //     },
+  //     {
+  //       name: 'Ontario Teachers Pension Plan',
+  //       fileName: 'otpp.webp',
+  //       url: 'https://www.otpp.com/en-ca/careers',
+  //     },
+  //   ],
+  // },
   {
-    size: 8.25,
+    size: 12,
     gap: 2,
     tier: "bronze",
     items: [
+      {
+        name: 'Best Buy',
+        fileName: 'bestbuy.webp',
+        url: 'https://www.bestbuy.com/',
+      }
+    ],
+  },
+  {
+    size: 5.25,
+    gap: 2,
+    tier: "bronze",
+    items: [
+      {
+        name: 'GAR',
+        fileName: 'gar.webp',
+        url: 'https://automationroboticsarduino.com/',
+      },
+      {
+        name: 'XYZ',
+        fileName: 'xyz.webp',
+        url: 'https://gen.xyz/',
+      },
       {
         name: 'Echo3D',
         fileName: 'echo3d.webp',
-        url: 'https://www.echo3d.co',
+        url: 'https://www.echo3d.com/',
       },
       {
-        name: 'StandOut Stickers',
-        fileName: 'standoutstickers.webp',
-        url: 'https://www.standoutstickers.com/',
-      },
-      {
-        name: 'Unity',
-        fileName: 'unity.webp',
-        url: 'https://unity.com/students',
+        name: 'Taskade',
+        fileName: 'taskade.webp',
+        url: 'https://www.taskade.com/',
       },
       {
         name: 'Voiceflow',
         fileName: 'voiceflow.webp',
         url: 'https://www.voiceflow.com/',
       },
+      {
+        name: 'Balsamiq',
+        fileName: 'balsamiq.webp',
+        url: 'https://balsamiq.com/',
+      }
     ],
   },
 ];
